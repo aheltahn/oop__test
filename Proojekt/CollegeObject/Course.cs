07504115f878778aeb24f860a0704f5f4cc3d0c5
Sevastopol <141613215+Sevastopol12@users.noreply.github.com>
2024-11-01 20:11:35 +0700

    Add files via upload

---
+++ Proojekt/CollegeObject/Course.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekt
+{
+    public class Course : Department
+    {
+        public string CourseCode { get; set; }
+        public string CourseName { get; set; }
+        public Lecturer Lecturer { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+
+        public List<Student> EnrolledStudents = new List<Student>();
+
+        public void AddStudent(Student student)
+        {
+            this.EnrolledStudents.Add(student); 
+        }
+        public void RemoveStudent(Student student)
+        {
+            this.EnrolledStudents.Remove(student);
+        }
+
+        public void AddLecturer(Lecturer lecturer)
+        {
+            this.Lecturer = lecturer;
+        }
+    }
+}
---
+++ Proojekt/Hooman/Human.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekt
+{
+    public abstract class Human
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime Bday { get; set; }
+        public Boolean Gender { get; set; } 
+        public string ClassID { get; set; }
+
+        public abstract void DisplayInfo();
+    }
+}
