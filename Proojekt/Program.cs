b84aaf82760ab94a20e7a2905155d493f040ab53
giangdang12378 <giangdang12378@gmail.com>
2024-11-02 21:38:13 +0700

    Program.cs

--- Proojekt/Program.cs
+++ Proojekt/Program.cs
@@ -10,6 +10,9 @@ namespace projekt
     {
         static void Main(string[] args)
         {
+            // tìm kiếm theo ClassID rồi in ra
+            List<Student> result = majorClass.FindByClassID("IT0001");
+            majorClass.PrintStudents(result);
         }
     }
 }
